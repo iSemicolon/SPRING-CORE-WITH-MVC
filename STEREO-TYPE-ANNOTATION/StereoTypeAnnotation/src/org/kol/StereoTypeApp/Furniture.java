f2d175134c8d6afea7556ece791171128ca552d3
PALASH SAMANTA <43672035+iSemicolon@users.noreply.github.com>
2021-02-19 18:11:04 +0530

    Add files via upload

---
+++ STEREO-TYPE-ANNOTATION/StereoTypeAnnotation/src/org/kol/StereoTypeApp/Furniture.java
@@ -0,0 +1,12 @@
+package org.kol.StereoTypeApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Furniture {
+
+	public void sofa() {
+		System.out.println("Stanley !!!!");
+		
+	}
+}
